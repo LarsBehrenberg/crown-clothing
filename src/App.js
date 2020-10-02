bd1a374ca502421ab1796e2d276cef63a8ae25ed
Lars Behrenberg <l.behrenberg@gmail.com>
2020-10-02 09:26:47 +0900

    Implement sign up method with firebase

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 // Utils
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
 
     this.state = {
       currentUser: null,
@@ -26,10 +26,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({
-        currentUser: user,
-      })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          })
+        })
+        console.log(this.state)
+      } else {
+        this.setState({ currentUser: userAuth })
+      }
     })
   }
 
