a3637d783911c0f96cdaa70b8e015d36cc55a613
Lars Behrenberg <l.behrenberg@gmail.com>
2020-10-09 11:34:43 +0900

    add cart is empty message

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -15,9 +15,11 @@ import { selectCartItems } from '../../redux/cart/cart.selectors'
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      {cartItems.map((item) => (
-        <CartItem key={item.id} item={item} />
-      ))}
+      {cartItems.length ? (
+        cartItems.map((item) => <CartItem key={item.id} item={item} />)
+      ) : (
+        <span className="empty-message">Your cart is empty</span>
+      )}
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
